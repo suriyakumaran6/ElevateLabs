bb4368bf9a43596bc4739dbcb3ff334c91f27520
Suriya Kumaran <suriyakumarananbazhagan@gmail.com>
2025-10-25 22:31:55 +0530

    Create config.py

---
+++ Project/config.py
@@ -0,0 +1,8 @@
+# config.py
+# Edit these for your environment / lab target
+TARGET = "http://localhost:3000"   # Default: Juice Shop if running locally
+MAX_DEPTH = 2
+RATE_LIMIT_SEC = 0.5
+ALLOWED_HOSTS = ["localhost", "127.0.0.1"]
+DB_PATH = "scanner.db"
+USER_AGENT = "Suriya-Scanner/1.0"
